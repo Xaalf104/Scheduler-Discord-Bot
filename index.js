1dbfe21f7301441163b4bbdcd77c7cea418ba1e6
Xaalf104 <alsharief.dinglasa06@gmail.com>
2024-07-07 17:05:58 +0800

    added /create-schedule command

--- index.js
+++ index.js
@@ -1,48 +1,109 @@
-require('dotenv').config();
-const { Client, IntentsBitField } = require('discord.js');
+require("dotenv").config();
+const {
+  Client,
+  IntentsBitField,
+  StringSelectMenuOptionBuilder,
+  ActionRowBuilder,
+  StringSelectMenuBuilder,
+} = require("discord.js");
 
 const client = new Client({
-    intents: [
-        IntentsBitField.Flags.Guilds,
-        IntentsBitField.Flags.GuildMembers,
-        IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.MessageContent
-    ]
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
 });
 
-client.login(process.env.DISCORD_BOT_TOKEN)
-    .then(() => {
-        console.log('Successfully logged in!');
-    })
-    .catch((error) => {
-        console.error('Failed to log in:', error);
-    });
+client
+  .login(process.env.DISCORD_BOT_TOKEN)
+  .then(() => {
+    console.log("Successfully logged in!");
+  })
+  .catch((error) => {
+    console.error("Failed to log in:", error);
+  });
 
-client.on('error', (error) => {
-    console.error('An error occurred with the Discord client:', error);
+client.on("error", (error) => {
+  console.error("An error occurred with the Discord client:", error);
 });
 
-client.on('ready', (c) =>{
-    console.log("Ready");
-})
-
-client.on('messageCreate', (message) => {
-    if (message.author.bot){
-        return;
-    }
+client.on("ready", (c) => {
+  console.log("Ready");
 });
 
-process.on('unhandledRejection', (reason, promise) => {
-    console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+client.on("messageCreate", (message) => {
+  if (message.author.bot) {
+    return;
+  }
 });
 
-process.on('uncaughtException', (error) => {
-    console.error('Uncaught Exception thrown:', error);
+process.on("unhandledRejection", (reason, promise) => {
+  console.error("Unhandled Rejection at:", promise, "reason:", reason);
+});
+
+process.on("uncaughtException", (error) => {
+  console.error("Uncaught Exception thrown:", error);
 });
 
 //interactions
 
-client.on('interactionCreate', (interaction) => {
-    if(!interaction.isChatInputCommand()) return;
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
 
-})
+  if (interaction.commandName === "create-schedule") {
+    await handleCreateScheduleCommand(interaction);
+  }
+});
+
+async function handleCreateScheduleCommand(interaction) {
+  const days = [
+    {
+      label: "Monday",
+      value: "monday",
+    },
+    {
+      label: "Tuesday",
+      value: "tuesday",
+    },
+    {
+      label: "Wednesday",
+      value: "wednesday",
+    },
+    {
+      label: "Thursday",
+      value: "thursday",
+    },
+    {
+      label: "Friday",
+      value: "friday",
+    },
+    {
+      label: "Saturday",
+      value: "saturday",
+    },
+    {
+      label: "Sunday",
+      value: "sunday",
+    },
+  ];
+  const selectMenu = new StringSelectMenuBuilder()
+    .setCustomId(interaction.id)
+    .setPlaceholder("Make a selection")
+    .setMinValues(1)
+    .setMaxValues(7)
+    .addOptions(
+      days.map((day) =>
+        new StringSelectMenuOptionBuilder()
+          .setLabel(day.label)
+          .setValue(day.value)
+      )
+    );
+
+  const actionRow = new ActionRowBuilder().addComponents(selectMenu);
+
+  interaction.reply({
+    components: [actionRow],
+  });
+}
--- register-commands.js
+++ register-commands.js
@@ -1,39 +1,48 @@
-require('dotenv').config();
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-const {ApplicationCommandOptionType} = require('discord.js')
+require("dotenv").config();
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+const { ApplicationCommandOptionType } = require("discord.js");
 
 const commands = [
-    {
-        name: 'create-schedule',
-        description: 'Create a schedule for this week or today',
-        options:[
-            {
-                name: 'week',
-                description: 'Set a schedule for this week.',
-                type: ApplicationCommandOptionType.Number,
-            },{
-                name: 'today',
-                description: 'Set a schedule today.',
-                type: ApplicationCommandOptionType.Number,
-            }
-        ]
-    },
+  {
+    name: "create-schedule",
+    description: "Create a schedule for this week or today",
+    options: [
+      {
+        name: "type",
+        description: "Set a schedule for this week or today.",
+        type: ApplicationCommandOptionType.String,
+        required: true,
+        choices: [
+          {
+            name: "Week",
+            value: "week",
+          },
+          {
+            name: "Today",
+            value: "today",
+          },
+        ],
+      },
+    ],
+  },
 ];
 
-const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_BOT_TOKEN);
+const rest = new REST({ version: "10" }).setToken(
+  process.env.DISCORD_BOT_TOKEN
+);
 
 (async () => {
-    try {
-        console.log('Registering commands...');
-        
-        await rest.put(
-            Routes.applicationGuildCommands(process.env.CLIENT, process.env.GUILD),
-            { body: commands }
-        );
+  try {
+    console.log("Registering commands...");
 
-        console.log('Commands registered successfully');
-    } catch (error) {
-        console.error(`An error occurred: ${error.message}`);
-    }
+    await rest.put(
+      Routes.applicationGuildCommands(process.env.CLIENT, process.env.GUILD),
+      { body: commands }
+    );
+
+    console.log("Commands registered successfully");
+  } catch (error) {
+    console.error(`An error occurred: ${error.message}`);
+  }
 })();
